01edabd38cfe2f76414ab26071116f0a2af02de6
Becky <bckyjns@gmail.com>
2023-03-05 22:17:06 +0000

    add keymap descriptions

--- after/plugin/keymap.lua
+++ after/plugin/keymap.lua
@@ -19,8 +19,8 @@ vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 -- format current file
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = "Format current buffer" })
 
-vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Toggle undotree" })
 
-vim.keymap.set("n", "<leader>t", vim.cmd.Neotree)
+vim.keymap.set("n", "<leader>t", vim.cmd.Neotree, { desc = "Open file tree" })
