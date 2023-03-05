8a4d91e0015feb22d7ff232512b1636135b2f96f
Becky <bckyjns@gmail.com>
2023-03-05 17:39:01 +0000

    keymaps

---
+++ lua/custom/keymap.lua
@@ -0,0 +1,22 @@
+-- move highlighted text up & down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- when applying the next line to the current line, keeps cursor in the
+-- same place instead of moving it to the new end of the line
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- keeps cursor centred when doing page jumping
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- keeps cursor centred when paging through search results
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- yank into system clipboard
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- format current file
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
